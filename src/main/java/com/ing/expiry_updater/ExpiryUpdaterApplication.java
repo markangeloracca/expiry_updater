ac217e7738c38a40975d9796bd02f40a7906bc26
markangeloracca <43604363+markangeloracca@users.noreply.github.com>
2024-10-25 16:39:22 +0800

    Added spring boot project structure

---
+++ src/main/java/com/ing/expiry_updater/ExpiryUpdaterApplication.java
@@ -0,0 +1,13 @@
+package com.ing.expiry_updater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpiryUpdaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpiryUpdaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ing/expiry_updater/ExpiryUpdaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.expiry_updater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpiryUpdaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
